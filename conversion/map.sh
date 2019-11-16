3dc67dcb7bd283798fa1138b44a71f29e63dea72
Sam <tritoke@protonmail.com>
2019-11-16 20:34:46 +0000

    Converted latest images.

--- conversion/map.sh
+++ conversion/map.sh
@@ -2,21 +2,16 @@
 
 SRC=$(realpath $1)
 DEST=$(realpath $2)
-echo "SRC = $SRC"
-echo "DEST = $DEST"
 
 for zip in $(ls $SRC/*.zip); do
   zname=$(basename ${zip%.*})
-  echo "zname = $zname"
   unzipdir=$SRC/$zname
-  echo "unzipdir = $unzipdir"
   [ -d $unzipdir ] || mkdir $unzipdir
-  yes | unzip $zip -d $unzipdir
+  yes | unzip $zip -d $unzipdir &> /dev/null
 
   destdir=$DEST/$zname
-  echo "destdir = $destdir"
   [ -d $destdir ] || mkdir $destdir
   for file in $(ls $unzipdir); do
-    ./convert.py $unzipdir/$file $destdir/${file%.*}.txt
+    ./convert.py $unzipdir/$file $destdir/${file%.*}.c
   done
 done
