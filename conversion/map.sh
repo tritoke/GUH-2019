023a40cee4c4b17ed6005dd682e862d857c1b13d
Sam <tritoke@protonmail.com>
2019-11-16 16:59:28 +0000

    Finished conversion script.

--- conversion/map.sh
+++ conversion/map.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-for file in $(ls $1)
-do
-  ./convert.py $1/$file $2/${file%.*}.txt
+
+SRC=$(realpath $1)
+DEST=$(realpath $2)
+echo "SRC = $SRC"
+echo "DEST = $DEST"
+
+for zip in $(ls $SRC/*.zip); do
+  zname=$(basename ${zip%.*})
+  echo "zname = $zname"
+  unzipdir=$SRC/$zname
+  echo "unzipdir = $unzipdir"
+  [ -d $unzipdir ] || mkdir $unzipdir
+  yes | unzip $zip -d $unzipdir
+
+  destdir=$DEST/$zname
+  echo "destdir = $destdir"
+  [ -d $destdir ] || mkdir $destdir
+  for file in $(ls $unzipdir); do
+    ./convert.py $unzipdir/$file $destdir/${file%.*}.txt
+  done
 done
