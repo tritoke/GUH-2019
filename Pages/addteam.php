4459c8aa71f7878756c1094263c4e438bed51bc9
bensismith <bensismith2001@gmail.com>
2019-11-17 11:31:43 +0000

    Start of addteam

---
+++ Pages/addteam.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bin Battle Add Team</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="General.css">
+
+</head>
+<body>
+	<!--Navbar/Header (Stick on viewpoint-->
+
+	<div class="header">
+	
+		<a href="index.php">
+			<img style="float: left;" src="home.png">
+		</a>
+		<h1>Login</h1>
+	</div>
+	
+	<div class="col login">
+		<table style="position: middle;">
+		<form action="####.php" method="post">
+			<tr>
+				<th>Team Name:</th>
+				<th><input type="text" name="team-name"></th>
+			</tr>
+				<th><input type="submit" value="Submit"></th>
+				<th></th>
+			</form>
+		</table>
+		<br>
+	</div>
+
+	<div class="footer">
+		<img src="facebook.png">
+		<img src="twitter.png">
+		<img src="linkedin.png">
+		<img src="insta.png">
+	</div>
+</body>
+</html>
