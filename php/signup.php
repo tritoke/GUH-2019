10099312848dc15f15d13ca6c2e7b00ce5ec7005
Sam <tritoke@protonmail.com>
2019-11-17 08:14:12 +0000

    more API stuff

--- php/signup.php
+++ php/signup.php
@@ -4,37 +4,17 @@
   $conn = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
   
   $username = $_POST['username'];
+  //$username = $_GET['username'];
 
-  if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
-    echo "<h1>INVALID EMAIL FUCK OFF</h1>";
-    exit(0);
-  }
+  if (!filter_var($username, FILTER_VALIDATE_EMAIL))
+    die("Invalid email.");
 
   $passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  //$passwordHash = password_hash($_GET['password'], PASSWORD_DEFAULT);
 
-  $teamname = $_POST['teamname'];
-  $stmt = $conn->prepare("SELECT TeamNumber FROM TeamInfo WHERE TeamName = ?");
-  $stmt->bind_param('s', $teamname);
-
-  $stmt->execute();
-
-  $res = $stmt->get_result();
-  if ($res) {
-    $teamnumber = $res->fetch_assoc();
-  } else {
-    echo "Couldn't find team number from team name";
-    exit(0);
-  }
-
-  $stmt = $conn->prepare("INSERT INTO EmployeeInfo (Email, PasswordHash, ScoreFood, ScorePaper, ScoreMixedRecyling, ScorePlasticBottles, ScoreBatteries) VALUES");
-  $stmt->bind_param('s', $username);
+  $stmt = $conn->prepare("INSERT INTO EmployeeInfo (Email, PasswordHash) VALUES (?, ?)");
+  $stmt->bind_param("ss", $username, $passwordHash);
 
   $stmt->execute();
-  $hash = $stmt->get_result()->fetch_assoc();
-  
-  if (password_verify($password, $hash)) {
-    echo "valid";
-  } else {
-    echo "invalid";
-  }
+  $stmt->close();
 ?>
